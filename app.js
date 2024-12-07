9a0e4af76b98911a97b593c3d1360bfab716ec82
humaidshaykh <humaidshaykh@gmail.com>
2024-12-07 19:52:49 +0500

    In this code we cover cookieParser, bcrypt, jwt

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
     // res.cookie('name', "harsh");
 
-    bcrypt.compare("humaids", "$2b$10$qpUOX/nROf9o88kfIHdGw.gMgAFlvCSqEb.J7CXoDOFrF5lnebh4C", function(err, result) {
-        console.log(result);
-    });
+    // bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash("humaids", salt, function(err, hash) {
+    //         console.log(hash);
+    //     });
+    // });
 
-    res.send("done");
+    // bcrypt.compare("humaids", "$2b$10$qpUOX/nROf9o88kfIHdGw.gMgAFlvCSqEb.J7CXoDOFrF5lnebh4C", function(err, result) {
+    //     console.log(result);
+    // });
+
+    let = token = jwt.sign({email: "hs@gmail.com"}, "secret");
+    res.cookie("token", token);
+    res.send("done")
+    
+});
+
+app.get('/read', (req, res) => {
+    console.log(req.cookies.token);
+    res.send("read page");
+
+    const data = jwt.verify(req.cookies.token ,  "secret");
+    console.log(data);
+    
 });
 
 // app.get('/read', (req, res) => {
